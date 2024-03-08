88b270984b5ca6b9e37d1875e90d998eb90b4ce2
indura08 <induraperera3@gmail.com>
2024-03-09 04:01:57 +0530

    first commit

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weigth: 500;
+`
+
+const Announcement = () => {
+  return (
+    <div>
+      <Container>
+        Todays best seller Ranking - check it out : click here!
+      </Container>
+    </div>
+  )
+}
+
+export default Announcement
---
+++ src/components/Slider.jsx
@@ -0,0 +1,40 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100%;
+    height : 100vh;
+    display: flex;
+    background-color: coral;
+
+`
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff7f7;
+    border-radius: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+
+const Slider = () => {
+  return (
+    <div>
+      <Container>
+        <Arrow>
+             <ArrowLeftOutlined/>
+        </Arrow>
+
+        <Arrow>
+             <ArrowRightOutlined/>
+        </Arrow>
+      </Container>
+    </div>
+  )
+}
+
+export default Slider
+// 30:05n nawattuwe
