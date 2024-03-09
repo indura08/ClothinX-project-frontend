dd3ee0f2d0aa5ed2a951d264924b709418b078ba
indura08 <induraperera3@gmail.com>
2024-03-10 01:43:41 +0530

    data.js first commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "sliderimg1.jpg",
+        title: "Take 80% off with Summer Sale",
+        desc: "Dont't wait, Grab your favourite 5 items and get the 80% off discount",
+        bg: "white",
+    },
+
+    {
+        id: 2,
+        img: "slider2.jpg",
+        title: "Seasonal Offers",
+        desc: "Buy seasonal outfits and stand a chance to win $50 voucher",
+        bg: "F5D9FE",
+    },
+
+    {
+        id: 3,
+        img: "slider2.jpg",
+        title: "New Collection",
+        desc: "Explore new collections from brand POLO, libra and annation",
+        bg: "FAF0FD",
+    },
+];
