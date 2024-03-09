111bcd0c296aecceb599430a7012ace81a3527a8
indura08 <induraperera3@gmail.com>
2024-03-10 01:40:24 +0530

    third commit for Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
 import styled from "styled-components"
 import { useState } from "react"
+import { sliderItems } from "../data"
 
 const Container = styled.div`
     width: 100%;
@@ -9,67 +10,59 @@ const Container = styled.div`
     background-color: white;
     position: relative;
     overflow: hidden;
-
-`
-
+`;
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
     background-color: #fff7f7;
-    border-radius: 50px;
+    border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
     position: absolute;
     top: 0;
     bottom: 0;
-    left: ${props => props.direction === "left" && "10px"};
-    right: ${props => props.direction === "right" && "10px"};
+    left: ${(props) => props.direction === "left" && "10px"};
+    right: ${(props) => props.direction === "right" && "10px"};
     margin: auto;
     cursor: pointer;
     opacity: 0.7;
     z-index: 2;
-
-`
-
+`;
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transform: translateX(0)
-`
-
+  transform: translateX(${(props)=> props.slideIndex * -100} vw);
+`;
 const Slide =styled.div`
   width: 100vw;
   height: 100vh;
   display:flex;
   align-items: center;
-  background-color: #${props => props.bg};
-`
-
+  background-color: #${(props) => props.bg};
+`;
 const ImgContainer =styled.div`
   height: 100%;
   flex: 1;
-`
-
+`;
 const Image = styled.img`
   height: 100%;
   width: 140vh
 
-`
+`;
 const InfoContainer =styled.div`
   flex: 1;
   padding: 50px;
-`
-
+`;
 const Title = styled.h1`
   font-size: 70px;
-`
+`;
 const Desc = styled.p`
   margin: 50px, 0px;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
-`
+`;
 const Button = styled.button`
   padding: 10px;
   font-size: 20px;
@@ -83,13 +76,17 @@ const Button = styled.button`
     background-color: #E900FF;
     color: white;
   }
-`
-
+`;
 
 const Slider = () => {
+
   const [slideIndex, setSlideIndex] = useState(0)
   const handleClick = (direction) => {
-    
+    if(direction === "left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+    }else{
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0) 
+    }
   }
   return (
     <div>
@@ -98,49 +95,24 @@ const Slider = () => {
              <ArrowLeftOutlined/>
         </Arrow>
         
-        <Wrapper>
-          <Slide bg="white">
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map((item) => (
 
-          <ImgContainer>
-            <Image src = "sliderimg1.jpg" />
-          </ImgContainer>
+              <Slide bg={item.bg} key={item.id}>
 
-          <InfoContainer>
-            <Title>Take 80% off With Summer sale</Title>
-            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
-            <Button>SHOP NOW.</Button>
-          </InfoContainer>
+              <ImgContainer>
+                <Image src = {item.img} />
+              </ImgContainer>
 
-          </Slide>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOP NOW.</Button>
+              </InfoContainer>
 
-          <Slide bg="F5D9FE">
-
-          <ImgContainer>
-            <Image src = "sliderimg1.jpg" />
-          </ImgContainer>
-
-          <InfoContainer>
-            <Title>Take 80% off With Summer sale</Title>
-            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
-            <Button>SHOP NOW.</Button>
-          </InfoContainer>
-
-          </Slide>
-
-          <Slide bg="FAF0FD">
-
-          <ImgContainer>
-            <Image src = "sliderimg1.jpg" />
-          </ImgContainer>
-
-          <InfoContainer>
-            <Title>Take 80% off With Summer sale</Title>
-            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
-            <Button>SHOP NOW.</Button>
-          </InfoContainer>
-
-          </Slide>
-
+              </Slide>
+          ))}
+          
         </Wrapper>
 
         <Arrow direction="right" onClick={() => handleClick("right")}>
