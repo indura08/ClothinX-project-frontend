8c5b5463f89513db7ac09d393677e3c80ad03178
indura08 <induraperera3@gmail.com>
2024-03-09 20:21:18 +0530

    second commit for slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
 import styled from "styled-components"
+import { useState } from "react"
 
 const Container = styled.div`
     width: 100%;
     height : 100vh;
     display: flex;
-    background-color: coral;
+    background-color: white;
+    position: relative;
+    overflow: hidden;
 
 `
 
@@ -17,18 +20,130 @@ const Arrow = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.7;
+    z-index: 2;
 
 `
 
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(0)
+`
+
+const Slide =styled.div`
+  width: 100vw;
+  height: 100vh;
+  display:flex;
+  align-items: center;
+  background-color: #${props => props.bg};
+`
+
+const ImgContainer =styled.div`
+  height: 100%;
+  flex: 1;
+`
+
+const Image = styled.img`
+  height: 100%;
+  width: 140vh
+
+`
+const InfoContainer =styled.div`
+  flex: 1;
+  padding: 50px;
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+`
+const Desc = styled.p`
+  margin: 50px, 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  transition: 1s;
+  color: #E900FF;
+  border-radius: 50px;
+
+  &:hover {
+    background-color: #E900FF;
+    color: white;
+  }
+`
+
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0)
+  const handleClick = (direction) => {
+    
+  }
   return (
     <div>
       <Container>
-        <Arrow>
+        <Arrow direction="left" onClick={() => handleClick("left")}>
              <ArrowLeftOutlined/>
         </Arrow>
+        
+        <Wrapper>
+          <Slide bg="white">
 
-        <Arrow>
+          <ImgContainer>
+            <Image src = "sliderimg1.jpg" />
+          </ImgContainer>
+
+          <InfoContainer>
+            <Title>Take 80% off With Summer sale</Title>
+            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
+            <Button>SHOP NOW.</Button>
+          </InfoContainer>
+
+          </Slide>
+
+          <Slide bg="F5D9FE">
+
+          <ImgContainer>
+            <Image src = "sliderimg1.jpg" />
+          </ImgContainer>
+
+          <InfoContainer>
+            <Title>Take 80% off With Summer sale</Title>
+            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
+            <Button>SHOP NOW.</Button>
+          </InfoContainer>
+
+          </Slide>
+
+          <Slide bg="FAF0FD">
+
+          <ImgContainer>
+            <Image src = "sliderimg1.jpg" />
+          </ImgContainer>
+
+          <InfoContainer>
+            <Title>Take 80% off With Summer sale</Title>
+            <Desc>Dont wait, Grab your favourite 5 items once and get the 80% off discount</Desc>
+            <Button>SHOP NOW.</Button>
+          </InfoContainer>
+
+          </Slide>
+
+        </Wrapper>
+
+        <Arrow direction="right" onClick={() => handleClick("right")}>
              <ArrowRightOutlined/>
         </Arrow>
       </Container>
@@ -36,5 +151,4 @@ const Slider = () => {
   )
 }
 
-export default Slider
-// 30:05n nawattuwe
+export default Slider //nawattuwe 44:30
