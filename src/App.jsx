917403ed7d1ed9066c8c70160ced337c25fd5d0f
indura08 <induraperera3@gmail.com>
2024-03-12 03:32:55 +0530

    Minor App.jsx and data.js updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
 
 function App() {
   
   return (
     <>
-      <Home/>
+      <Product/>
     </>
   )
 }
